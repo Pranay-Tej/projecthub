4121f6cfa202501a4ed257581e93ce0b92f96565
pranay_teja <s.pranayteja@gmail.com>
2021-04-08 00:15:15 +0530

    show repos for selected project

--- ui-angular/src/app/projects/projects.module.ts
+++ ui-angular/src/app/projects/projects.module.ts
@@ -1,16 +1,16 @@
-import { ProjectFacade } from './store/project.facade';
-import { ProjectService } from './services/project.service';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
-import { ProjectListComponent } from './project-list/project-list.component';
 import { RepoListComponent } from './repo-list/repo-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ProjectRepoService } from './services/project-repo.service';
+import { ProjectService } from './services/project.service';
 import { RepoService } from './services/repo.service';
+import { ProjectFacade } from './store/project.facade';
 import { RepoFacade } from './store/repo.facade';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProjectsComponent, ProjectListComponent, RepoListComponent],
@@ -20,6 +20,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProjectsRoutingModule,
     ReactiveFormsModule,
   ],
-  providers: [ProjectService, ProjectFacade, RepoService, RepoFacade],
+  providers: [
+    ProjectService,
+    ProjectFacade,
+    RepoService,
+    RepoFacade,
+    ProjectRepoService,
+  ],
 })
 export class ProjectsModule {}
--- ui-angular/src/app/projects/store/repo.facade.ts
+++ ui-angular/src/app/projects/store/repo.facade.ts
@@ -1,18 +1,35 @@
+import { ProjectFacade } from './project.facade';
+import { ProjectRepoService } from './../services/project-repo.service';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { RepoService } from '../services/repo.service';
 
 @Injectable()
 export class RepoFacade {
-  constructor(private repoService: RepoService) {}
+  constructor(
+    private repoService: RepoService,
+    private projectRepoService: ProjectRepoService,
+    private projectFacade: ProjectFacade
+  ) {}
 
   private repoList = new Subject();
 
   repoList$ = this.repoList.asObservable();
 
   getAllRepos() {
+    this.projectFacade.setSelectedProject('all');
     this.repoService
       .getAllRepos()
       .subscribe((data) => this.repoList.next(data));
+  }
+
+  getProjectRepos(projectId: string) {
+    this.projectFacade.setSelectedProject(projectId);
+    this.projectRepoService
+      .getProjectRepos(projectId)
+      .subscribe((data: any) => {
+        let projectRepos = data.map((projectRepo) => projectRepo.repoId);
+        this.repoList.next(projectRepos);
+      });
   }
 }
